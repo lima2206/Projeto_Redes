79623eec24f1780963e9b68e749638788ae06953
lima2206 <vitor.lima2206@gmail.com>
2024-11-26 01:38:47 -0300

    Docker

--- redes/src/app/app.module.ts
+++ redes/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,19 +9,30 @@ import { join } from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql', // Change this to your DB type
-      port:3306,
-      username: 'user',
-      password:'Password132#',
-      database: 'redes',
-      entities: [Item],
-      synchronize: true,
+    // Import ConfigModule to load .env variables
+    ConfigModule.forRoot({
+      isGlobal: true,  // Makes config globally available
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT'), 10),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: 'redes',
+        entities: [Item],
+        synchronize: true,  // Set to false for production to avoid data loss
+      }),
+      inject: [ConfigService],
+    }),
+
     TypeOrmModule.forFeature([Item]),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'src/frontend'),
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
